43df1064f3c622d6ab1e7afd737ba19d53db11f9
chenzhiyong <172471067@qq.com>
2026-01-07 14:40:36 +0800

    添加阻止系统休眠和锁屏功能。

--- InputSimulator/simulator/windowsconsole.cpp
+++ InputSimulator/simulator/windowsconsole.cpp
@@ -1,11 +1,14 @@
 #include "windowsconsole.h"
 #include <iostream>
+#include <windows.h>
 WindowsConsole::WindowsConsole()
 {
+	SetThreadExecutionState(ES_SYSTEM_REQUIRED | ES_DISPLAY_REQUIRED | ES_CONTINUOUS);
 }
 
 WindowsConsole::~WindowsConsole()
 {
+	SetThreadExecutionState(ES_CONTINUOUS);
 }
 
 void WindowsConsole::log(const std::string& text)
